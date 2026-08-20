7791825152aa311355f58a2094a11694461a9a48
Jan Vogt <jan.vogt06@web.de>
2026-08-20 15:46:49 +0200

    Add frontend configuration with proxy placeholder

---
+++ site/js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  corsProxies: [
+    'https://fupa-proxy.DEIN-SUBDOMAIN.workers.dev/?url={url}',
+  ],
+  imageVariant: '1920xauto.jpeg',
+  fileExtension: 'jpg',
+  concurrency: 5,
+};
